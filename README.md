8eb9b4ec86df5f49566abe653159e3f04e391ee7
hyliang96 <hyliang96@hotmail.com>
2019-10-03 00:32:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# 自制mac的v2ray终端管理器
+
+## 功能
+
+在终端管理v2ray-core，切换不同的配置，并实现终端、浏览器的v2ray代理
+
+在开、关v2ray-core前，它会做以下事情
+
+- 在mac的`系统偏好设置-网络-高级-代理`中设置，开启、关闭`http`及`https`的代理，从而让浏览器能走、不走代理
+- 设置或删除 `http(s)_proxy`，从而让终端能走、不走代理
+- 在v2ray-core启动前，关闭其他代理客户端（shadowsocksX-NG-R、V2RayU）以避免它们占用本方的v2ray-core需要的端口
+
+## 安装v2ray-core
+
+安装v2ray-core
+
+```bash
+brew install v2ray-core
+```
+
+配置文件在
+
+```bash
+/usr/local/etc/v2ray
+```
+
+## 终端管理器使用方法
+
+将以下添加到`~/.bash_profile`之类的登录即加载文件中
+
+```bash
+. ./v2ray_client.sh
+```
+
+而后终端下，执行如下命令来管理v2ray-core进程
+
+```bash
+v2  [参数]
+```
+
+参数：
+
+`v2`                 : 开启当前默认配置
+`v2 start <配置名>`  : 修改当前默认配置为<配置名>，并重启v2ray
+`v2 stop`            : 停止v2ray
+`v2 list`            : 查看所有配置
+`v2 status|log`      : 查看当前日志
+`v2 jch|ps`          : 查看当前v2ray进程
+
+## ToDo
+
+### 实现开机自启
+
+尝试了一下方法，但还无法实现开机自启
+
+撰写一个bash脚本，位于`/path/to/LoginHook/script`，其中有一行为
+
+```bash
+/绝对路径到本项目/v2ray_deamon.sh
+```
+
+添加自启项
+
+```bash
+sudo defaults write com.apple.loginwindow LoginHook /path/to/LoginHook/script
+```
+
+撤销自启项
+
+```bash
+sudo defaults delete com.apple.loginwindow LoginHook
+```
+
+对应的文件位于`/var/root/Library/Preferences/com.apple.loginwindow.plist`
+
+注：整个OSX只有一个loginhook（更多参见[apple官方文档](https://developer.apple.com/library/archive/technotes/tn2228/_index.html)- "Login Hook"）
+
+更多参见：
+
+https://support.apple.com/en-us/HT2420
+
+https://blog.csdn.net/github_35041937/article/details/52709098
+
+https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CustomLogin.html 
---
+++ v2ray_client.sh
@@ -0,0 +1,175 @@
+#!/usr/bin/env bash
+
+v2ray_client_log='/Users/mac/Desktop/v2ray/log'
+v2ray_command='v2ray'
+v2ray_config_dir='/usr/local/etc/v2ray'
+v2ray_http_port=1087
+
+
+#  翻墙代理设置，使得命令行下可以翻墙
+# 国内网站不代理
+export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com,.souche.com"
+# ss, ssr, v2ray: 对curl, w3m有效，对ping无效
+fq()
+{
+    if [ $# -eq 0 ] || [ "$1" = 'start' ]; then
+        #  对curl有效，对w3m,ping 无效
+        # export http_proxy="socks5h://127.0.0.1:1080"
+        # export https_proxy="socks5h://127.0.0.1:1080"
+        # export ftp_proxy="socks5h://127.0.0.1:1080"
+
+        # 对curl, w3m有效，对ping无效
+        export http_proxy="http://127.0.0.1:$v2ray_http_port"
+        export https_proxy="http://127.0.0.1:$v2ray_http_port"
+        export ftp_proxy="http://127.0.0.1:$v2ray_http_port"
+        echo "终端开始翻墙"
+    elif [ "$1" = 'stop' ]; then
+        unset http_proxy
+        unset https_proxy
+        unset ftp_proxy
+        echo "终端结束翻墙"
+    else
+        echo 'fq       : 终端开始翻墙'
+        echo 'fq start : 终端开始翻墙'
+        echo 'fq stop  : 终端结束翻墙'
+    fi
+}
+
+
+
+v2_jch()
+{
+    ps aux | awk NR!=1 | grep " $v2ray_command " | grep -v grep
+}
+
+
+v2()
+{
+    if [ $# -eq 0 ] || [ "$1" = 'start' ]; then
+        if ! [ -d "$v2ray_config_dir" ]; then
+            echo "config dir not found: $v2ray_config_dir" >&2
+            return
+        fi
+
+        # config文件
+        local default_config="$v2ray_config_dir/config.json"
+        if [ "$1" = 'start' ]; then
+            local new_config="$v2ray_config_dir/$2.json"
+            if [ -f "$new_config" ]; then
+                if [ -f "$default_config" ]; then
+                    rm $default_config
+                fi
+                ln -s $new_config $default_config
+            else
+                echo "config not found: $new_config" >&2
+                return
+            fi
+        else
+            # 检查config.json存在
+            if ! [ -f "$default_config" ]; then
+                local configs=($v2ray_config_dir/*.json)
+                local new_config="${configs[1]}"
+                if [ -f "$new_config" ]; then
+                    ln -s $new_config $default_config
+                fi
+            fi
+        fi
+
+        # 检查依赖存在
+        if ! [ -f "$default_config" ]; then
+            echo "config not found: $default_config" >&2
+            return
+        fi
+
+        if ! ( command -v $v2ray_command > /dev/null ); then
+            echo "command not found: $v2ray_command" >&2
+            return
+        fi
+
+        touch $v2ray_client_log
+        if ! [ -f "$v2ray_client_log" ]; then
+            echo "log file not found: $v2ray_client_log" >&2
+            return
+        fi
+
+
+        # 关闭其他翻墙程序
+        if ! [ "`ps aux | grep -F 'ShadowsocksX-NG' | grep -v grep`" = '' ]; then
+            local _user=`ps aux | grep -F 'ShadowsocksX-NG' | grep -v grep | awk 'NR==1{print}' | awk '{print $1}'`
+            sudo su - $_user -c 'open -a  "ShadowsocksX-NG-R8"'
+            sudo su - $_user -c 'osascript -e "tell application \"ShadowsocksX-NG-R8\" to quit"'
+            sleep 3
+        fi
+
+
+        if ( ! [ "`ps aux | grep -F './v2ray-core/v2ray' | grep -v grep`" = '' ] ) || \
+           ( ! [ "`ps aux | grep -F 'V2rayU' | grep -v grep`" = '' ] ); then
+            local _user=`ps aux | grep -F 'V2rayU' | grep -v grep | awk 'NR==1{print}' | awk '{print $1}'`
+            if [ "$_user" = '' ]; then
+                local _user=`ps aux | grep -F './v2ray-core/v2ray' | grep -v grep | awk 'NR==1{print}' | awk '{print $1}'`
+            fi
+            sudo su - $_user -c 'open -a "V2RayU"'
+            sudo su - $_user -c 'osascript -e "tell application \"V2RayU\" to quit"'
+        fi
+
+        # 关闭之前的v2ray
+        v2 stop
+
+        # 开启v2ray
+        ( (
+            $v2ray_command  -config $default_config
+        ) & ) > $v2ray_client_log 2>&1
+        echo "v2ray 已启动"
+
+        # 系统http，https翻墙，可使所有浏览器翻墙
+        sudo networksetup -setwebproxy 'Wi-Fi' 127.0.0.1 $v2ray_http_port
+        sudo networksetup -setsecurewebproxy 'Wi-Fi' 127.0.0.1 $v2ray_http_port
+        echo "系统http, https代理开启，设为'127.0.0.1 $v2ray_http_port'"
+        # 终端翻墙
+        fq
+
+    elif [ "$1" = 'stop' ]; then
+        if ! [ "`v2_jch`" = '' ]; then
+            sudo killall $v2ray_command
+            if [ "`v2_jch`" = '' ]; then
+                echo "已关闭v2ray"
+            else
+                echo "未关闭v2ray"
+                v2_jch
+            fi
+        else
+            echo "no process named: $v2ray_command"
+        fi
+
+        # 系统http，https结束翻墙
+        sudo networksetup -setwebproxystate 'Wi-Fi' off
+        sudo networksetup -setsecurewebproxystate 'Wi-Fi' off
+        echo "系统http, https代理关闭"
+        # 终端结束翻墙
+        fq stop
+
+    elif [ "$1" = 'jch' ] || [ "$1" = 'ps' ]; then
+        ps aux | awk NR==1
+        v2_jch
+
+    elif [ "$1" = 'log' ] || [ "$1" = 'status' ]; then
+        cat $v2ray_client_log
+
+    elif [ "$1" = 'list' ]; then
+        ls -la $v2ray_config_dir
+
+    else
+        echo '`v2`                 : 开启当前默认配置'
+        echo '`v2 start <配置名>`  : 修改当前默认配置为<配置名>，并重启v2ray'
+        echo '`v2 stop`            : 停止v2ray'
+        echo '`v2 list`            : 查看所有配置'
+        echo '`v2 status|log`      : 查看当前日志'
+        echo '`v2 jch|ps`          : 查看当前v2ray进程'
+    fi
+
+    # unalias v2_jch
+}
+
+# unset -v v2ray_command
+# unset -v v2ray_http_port
+# unset -v v2ray_config_dir
